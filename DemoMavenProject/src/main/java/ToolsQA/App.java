45f4e2180db3abf7737e01e7399ec1cd737a686e
PrasadTarte <ptarte903@gmail.com>
2021-09-29 16:03:40 +0530

    All files are cleaned and committed.

--- DemoMavenProject/src/main/java/ToolsQA/App.java
+++ DemoMavenProject/src/main/java/ToolsQA/App.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package ToolsQA;
 
 /**
@@ -12,18 +11,3 @@ public class App
         System.out.println( "Hello World!" );
     }
 }
-=======
-package ToolsQA;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
->>>>>>> dac4aecffb210691b55ad6d6d07f52ab2be256c8
--- DemoMavenProject/src/test/java/ToolsQA/AppTest.java
+++ DemoMavenProject/src/test/java/ToolsQA/AppTest.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package ToolsQA;
 
 import junit.framework.Test;
@@ -37,43 +37,3 @@ public class AppTest
         assertTrue( true );
     }
 }
-=======
-package ToolsQA;
-
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-}
->>>>>>> dac4aecffb210691b55ad6d6d07f52ab2be256c8
